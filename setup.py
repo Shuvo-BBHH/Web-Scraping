3a84a5cf5135ded8408281002c66ecd2f874fc25
Mahdi Hasan Shuvo <98658558+Shuvo-BBHH@users.noreply.github.com>
2023-04-04 19:10:22 +0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
-setup(name="x-set",
-version='0.7',
+setup(name='x-set',
+version='0.8',
 description='this is a simple pip modul and test',
 long_description='THia is simpile modul crate by mahdi hasan',
 package=['x-set'],
